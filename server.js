5e9d459a6633eece12e367803964ea6a25b7060e
neerajsharma54321 <neerajsharma54321@gmail.com>
2020-05-14 23:49:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/trillo-app'));
+// app.use(express.static(__dirname + '/dist/trillo-app'));
+console.log(`I'm running the on application`);
+app.use(express.static(__dirname + '/dist'));
+res.sendFile(path.join(__dirname + '/dist/index.html'));
 
 app.get('/*', function(req,res) {
-    
+    console.log(`I'm in index.html file`);
 res.sendFile(path.join(__dirname+'/dist/trillo-app/index.html'));
 });
 
